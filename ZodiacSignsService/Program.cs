4a88853e24a26d2d3b924e76403dcc8882f10995
George <georgedusa12@gmail.com>
2021-04-17 11:14:23 +0300

    Code tweaks.

--- ZodiacSignsService/Program.cs
+++ ZodiacSignsService/Program.cs
@@ -13,7 +13,7 @@ namespace ZodiacSignsService
     {
         public static void Main(string[] args)
         {
-            //CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         // Additional configuration is required to successfully run gRPC on macOS.
